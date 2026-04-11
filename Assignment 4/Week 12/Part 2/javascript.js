499c6bf055688e879bb81829c6f5420ef14a816f
RaviHunter123 <ravi_hunter3@hotmail.com>
2026-04-10 21:34:08 -0400

    add image parameters

--- Assignment 4/Week 12/Part 2/javascript.js
+++ Assignment 4/Week 12/Part 2/javascript.js
@@ -25,4 +25,12 @@ const newImage = document.createElement("img");
  newImage.src = `${baseURL}${image.filename}`;
   newImage.alt = image.alt;
 
-  
+newImage.tabIndex = "0";
+thumbBar.appendChild(newImage);
+newImage.addEventListener("click", updateDisplayedImage);
+  newImage.addEventListener("keydown",) (e) => {}
+    if (e.code === "Enter") {}
+      updateDisplayedImage(e);
+
+      
+
