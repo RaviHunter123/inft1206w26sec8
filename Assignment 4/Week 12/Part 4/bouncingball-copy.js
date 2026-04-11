f0b0daa80c43f1e07150ea577792578249b44425
RaviHunter123 <ravi_hunter3@hotmail.com>
2026-04-11 13:04:26 -0400

    update ball drawing

--- Assignment 4/Week 12/Part 4/bouncingball-copy.js
+++ Assignment 4/Week 12/Part 4/bouncingball-copy.js
@@ -12,17 +12,24 @@ function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+const count = document.getElementById("count");
+
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
-class Ball {
-  constructor(x, y, velX, velY, color, size) {
+
+function Shape(x, y, velx, vely, exists) {
     this.x = x;
     this.y = y;
     this.velX = velX;
     this.velY = velY;
-    this.color = color;
-    this.size = size;
+    this.exists = size;
+  }
+
+  function Ball (x, y, velx, vely, color, size) {
+    Shape.call(this, x, y, velx, vely, true);
+this.color = color;
+this.size = size;
   }
 
   draw() {
